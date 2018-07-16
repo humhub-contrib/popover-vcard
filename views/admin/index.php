81276a7b80d2ee4fe6e89b1932f41fc438b5d7c4
Lucas Bartholemy <lucas@bartholemy.com>
2018-07-16 17:57:58 +0200

    Added some variable hints

--- views/admin/index.php
+++ views/admin/index.php
@@ -20,15 +20,91 @@ use yii\helpers\Html;
                 <br/>
                 <?= $form->field($model, 'userEnabled')->checkbox(); ?>
                 <?= $form->field($model, 'userContent')->textarea(['rows' => 15]); ?>
+
+
+                <div class="table-responsive">
+                    <table class="table table-bordered table-striped">
+                        <colgroup>
+                            <col class="col-xs-5">
+                            <col class="col-xs-7">
+                        </colgroup>
+                        <thead>
+                        <tr>
+                            <th>&nbsp;&nbsp;<?= Yii::t('PopoverVcardModule.base', 'Variable name'); ?></th>
+                            <th>&nbsp;&nbsp;Description</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row"><code>{{ user.email }}</code></th>
+                            <td><?= Yii::t('PopoverVcardModule.base', 'The email of this user.'); ?></td>
+                        </tr>
+                        <tr>
+                            <th scope="row"><code>{{ user.FIELDNAME }}</code></th>
+                            <td><?= Yii::t('PopoverVcardModule.base', 'You can use any field from the user model.'); ?></td>
+                        </tr>
+                        <tr>
+                            <th scope="row"><code>{{ profile.firstname }}</code></th>
+                            <td><?= Yii::t('PopoverVcardModule.base', 'The first name of the user.'); ?></td>
+                        </tr>
+                        <tr>
+                            <th scope="row"><code>{{ profile.lastname }}</code></th>
+                            <td><?= Yii::t('PopoverVcardModule.base', 'The last name of the user.'); ?></td>
+                        </tr>
+                        <tr>
+                            <th scope="row"><code>{{ profile.FIELDNAME }}</code></th>
+                            <td><?= Yii::t('PopoverVcardModule.base', 'Each available profile field.'); ?></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
             <div class="col-md-6">
                 <h4><?= Yii::t('PopoverVcardModule.base', 'Space VCard'); ?></h4>
                 <br/>
                 <?= $form->field($model, 'spaceEnabled')->checkbox(); ?>
                 <?= $form->field($model, 'spaceContent')->textarea(['rows' => 15]); ?>
+
+                <div class="table-responsive">
+                    <table class="table table-bordered table-striped">
+                        <colgroup>
+                            <col class="col-xs-5">
+                            <col class="col-xs-7">
+                        </colgroup>
+                        <thead>
+                        <tr>
+                            <th>&nbsp;&nbsp;<?= Yii::t('PopoverVcardModule.base', 'Variable name'); ?></th>
+                            <th>&nbsp;&nbsp;<?= Yii::t('PopoverVcardModule.base', 'Description'); ?></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row"><code>{{ space.name }}</code></th>
+                            <td><?= Yii::t('PopoverVcardModule.base', 'The name of this space.'); ?></td>
+                        </tr>
+                        <tr>
+                            <th scope="row"><code>{{ space.description }}</code></th>
+                            <td><?= Yii::t('PopoverVcardModule.base', 'The description of this space.'); ?></td>
+                        </tr>
+                        <tr>
+                            <th scope="row"><code>{{ memberCount }}</code></th>
+                            <td><?= Yii::t('PopoverVcardModule.base', 'The current member count.'); ?></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
         </div>
 
+        <br/>
+        <br/>
+        <p>
+            <?= Yii::t('PopoverVcardModule.base', 'See more information about the template language:'); ?>
+            <a href="https://twig.symfony.com/doc/2.x/templates.html" target="_blank">
+                <i class="fa fa-arrow-right"></i> <?= Yii::t('PopoverVcardModule.base', 'Twig for Template Designers'); ?>
+            </a>
+        </p>
 
         <div class="form-group">
             <?= Html::submitButton(Yii::t('base', 'Save'), ['class' => 'btn btn-primary', 'data-ui-loader' => '']) ?>
