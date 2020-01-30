58fdd6952fc09e455549ed6d5a234bbfb5b45019
HumHub Translations <info@humhub.com>
2020-01-30 15:35:49 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/fr/base.php
+++ messages/fr/base.php
@@ -19,5 +19,5 @@ return array (
   'VCard Popover' => '',
   'Variable name' => '',
   'You can use any field from the user model.' => '',
-  '{count} members' => '',
+  '{count} members' => '{count} membres',
 );
